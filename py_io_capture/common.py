fb4b074af0ab03f68bef37db7304995cde5cd56c
Ethan He <yfhe@ucdavis.edu>
2023-09-20 20:53:53 +0000

    use special string tokens for io_capture errors

--- py_io_capture/common.py
+++ py_io_capture/common.py
@@ -1,6 +1,14 @@
 import os
-
+from strenum import StrEnum
 
 MAX_REPORT_SIZE = int(os.getenv("MAX_REPORT_SIZE", 10))
 DUMP_FILE_NAME = os.getenv("DUMP_FILE_NAME", "py_io_capture_report.json")
 MAX_RECURRSION_LIMIT = int(os.getenv("MAX_RECURRSION_LIMIT", 20))
+
+
+# special tokens
+class PythonReportError(StrEnum):
+    OPTIONAL_ARG_ABSENT = "<OPTIONAL_ARG_ABSENT>"
+    RECURSION_LIMIT_EXCEEDED = "<RECURSION_LIMIT_EXCEEDED>"
+    UNKNOWN_FILE = "<UNKNOWN_FILE>"
+    UNABLE_TO_STRINGIFY = "<UNABLE_TO_STRINGIFY>"
