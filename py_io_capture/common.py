a7c4429c1b562091352fdc589bf7ccd2b0079ca0
Yifeng He <yfhe@ucdavis.edu>
2023-09-28 16:12:51 -0700

    set recurrsion limit to 50

--- py_io_capture/common.py
+++ py_io_capture/common.py
@@ -3,7 +3,7 @@ from strenum import StrEnum
 
 MAX_REPORT_SIZE = int(os.getenv("MAX_REPORT_SIZE", 10))
 DUMP_FILE_NAME = os.getenv("DUMP_FILE_NAME", "py_io_capture_report.json")
-MAX_RECURRSION_LIMIT = int(os.getenv("MAX_RECURRSION_LIMIT", 20))
+MAX_RECURRSION_LIMIT = int(os.getenv("MAX_RECURRSION_LIMIT", 50))
 
 
 # special tokens
